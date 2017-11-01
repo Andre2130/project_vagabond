2ee6b8cb941fdeaac244c4885606e45bcda94c64
victoriactemple <victoriactemple@gmail.com>
2017-11-01 17:40:25 -0400

    tweaking with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,56 @@
 User.destroy_all
 Post.destroy_all
-City.destory_all
+City.destroy_all
 
-users = []
-cities = []
-posts = []
-
-users << User.create!(
+    User.create!(
     email: "victoria@gmail.com", 
     password: "blahblah"
     )
 
-users << User.create!(
+    User.create!(
     email: "tom@gmail.com", 
     password: "blahblah"
     )
 
-users << User.create!(
+    User.create!(
     email: "andre@gmail.com", 
-    password: "blahblah"}
+    password: "blahblah")
 
-cities << City.create!(
+     parks = Post.create!(
+        title: 'Parks',
+        description: 'great outdoor space, beautiful parks',
+    )
+    
+     hotels = Post.create!(
+        title: 'Hotels',
+        description: 'amazing hotels - good prices and centrally located',
+    )
+    
+     nightLife = Post.create!(
+        title: 'Nightlife',
+        description: 'fun bars, lots of live music, good clubs',
+    )
+
+    City.create!(
     name: "Atlanta", 
-    image:"https://i.imgur.com/ma1Eup5.jpg"
+    image:"https://i.imgur.com/ma1Eup5.jpg",
+    posts: [parks, hotels, nightLife]
     )
 
-cities << City.create!(
+    City.create!(
     name: "London", 
-    image:"https://i.imgur.com/yahfVTm.jpg"
+    image:"https://i.imgur.com/yahfVTm.jpg",
+    posts: [parks, hotels, nightLife]
     )
 
-cities << City.create!(
+    City.create!(
     name: "San Francisco", 
-    image:"https://i.imgur.com/aFys868.jpg"
+    image:"https://i.imgur.com/aFys868.jpg",
+    posts: [parks, hotels, nightLife]
     )
 
 
-posts << Post.create!(
-    title: 'Parks',
-    description: 'great outdoor space, beautiful parks',
-)
 
-posts << Post.create!(
-    title: 'Hotels',
-    description: 'amazing hotels - good prices and centrally located',
-)
 
-posts << Post.create!(
-    title: 'Nightlife',
-    description: 'fun bars, lots of live music, good clubs',
-)
-
-cities.each do |city|
-    Post.create!({
-        title: post[:title],
-        description: post[:description]
-    })
-  
-end
 
 
