9c173d17fbf1f70734fd670036b5beaf87f27ddd
tombeau19 <thomas.beauregard@gmail.com>
2017-11-01 14:39:25 -0400

    NavBar styled

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,48 +2,53 @@ import React from 'react';
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const NavBarLinks = styled.div`
-display: flex;
-flex-direction: row;
-Justify-content: space-between;
-padding-right: 20px;
-padding-left: 10px;
-a{
-    text-decoration: none;
-    color: black;
-}
-`
-
 const NavBarContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     background-color: white;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`
 
-`
-const LogoImage = styled.div`
-    
-    padding-left: 10px;
-img{
+const LogoImage = styled.img`
     height: 80px;
-}
 `
+
+const NavBarLinkHolder = styled.div`
+    display: flex;
+    flex-direction: row;
+    Justify-content: space-between;
+    padding-right: 20px;
+    padding-left: 10px;
+`
+
+const NavBarLinks = styled.button`
+    font-family: 'Rubik', sans-serif;
+    font-size: 25px;
+    background: white;
+    border: none;
+    padding: 20px;
+    a {
+        text-decoration: none;
+        color: black;
+    } 
+    a:hover {
+        text-shadow: 1px 1px 2px
+    }
+`
+
 
 const NavBar = () => {
     return (
         <NavBarContainer>
-            <LogoImage>
-                <img src="http://brightnewt.com/wp-content/uploads/2014/06/icon1024.png" />
-            </LogoImage>
-            <NavBarLinks>
-                <div>
-                    <button><Link to="#">Sign-up</Link></button>
-                </div>
-                <div>
-                    <button><Link to="#">Log-in</Link></button>
-                </div>
-            </NavBarLinks>
+
+            <LogoImage src="http://brightnewt.com/wp-content/uploads/2014/06/icon1024.png" />
+
+            <NavBarLinkHolder>
+                    <NavBarLinks><Link to="#">Sign-up</Link></NavBarLinks>
+                    <NavBarLinks><Link to="#">Log-in</Link></NavBarLinks>
+            </NavBarLinkHolder>
+
         </NavBarContainer>
     );
 };
