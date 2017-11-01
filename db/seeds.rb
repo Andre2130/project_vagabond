52d26d3d561e73d290550cac3e35ac4c1a4ed7c0
tombeau19 <thomas.beauregard@gmail.com>
2017-11-01 17:53:16 -0400

    seeded database, removed posts from seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.destroy_all
-Post.destroy_all
 City.destroy_all
+# Post.destroy_all
 
     User.create!(
     email: "victoria@gmail.com", 
@@ -16,38 +16,35 @@ City.destroy_all
     email: "andre@gmail.com", 
     password: "blahblah")
 
-     parks = Post.create!(
-        title: 'Parks',
-        description: 'great outdoor space, beautiful parks',
-    )
-    
-     hotels = Post.create!(
-        title: 'Hotels',
-        description: 'amazing hotels - good prices and centrally located',
-    )
-    
-     nightLife = Post.create!(
-        title: 'Nightlife',
-        description: 'fun bars, lots of live music, good clubs',
-    )
-
     City.create!(
     name: "Atlanta", 
-    image:"https://i.imgur.com/ma1Eup5.jpg",
-    posts: [parks, hotels, nightLife]
+    image:"https://i.imgur.com/ma1Eup5.jpg"
     )
 
     City.create!(
     name: "London", 
-    image:"https://i.imgur.com/yahfVTm.jpg",
-    posts: [parks, hotels, nightLife]
+    image:"https://i.imgur.com/yahfVTm.jpg"
     )
 
     City.create!(
     name: "San Francisco", 
-    image:"https://i.imgur.com/aFys868.jpg",
-    posts: [parks, hotels, nightLife]
+    image:"https://i.imgur.com/aFys868.jpg"
     )
+
+    # Post.create!(
+    #     title: 'Parks',
+    #     description: 'great outdoor space, beautiful parks',
+    # )
+    
+    # Post.create!(
+    #     title: 'Hotels',
+    #     description: 'amazing hotels - good prices and centrally located',
+    # )
+    
+    # Post.create!(
+    #     title: 'Nightlife',
+    #     description: 'fun bars, lots of live music, good clubs',
+    # )
 
 
 
