b21b5d4777275aac8bbb63d14c73f88e00af6ed4
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-11-01 12:36:33 -0400

    created a homepage componentinstalled styled-component react router dom and axios

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                
+          <img src="http://brightnewt.com/wp-content/uploads/2014/06/icon1024.png" className="App-logo" alt="logo" />
+       
+            </div>
+        );
+    }
+}
+
+export default HomePage;
