839e7ae33c5e263b509744d6c96cbc43b18780ff
tombeau19 <thomas.beauregard@gmail.com>
2017-11-02 10:03:26 -0400

    reseeded with many to many relationship between cities and users - posts acts as the middle table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,75 @@
 User.destroy_all
 City.destroy_all
-# Post.destroy_all
+Post.destroy_all
 
-    User.create!({
-    email: "victoria@gmail.com", 
-    password: "blahblah"
-    })
+    users = []
+    cities = []
+    
+    5.times do 
+        users << User.create(
+            email: FFaker::Internet.email,
+            password: 'blahblah'
+        )
+    end
 
-    User.create!({
-    email: "tom@gmail.com", 
-    password: "blahblah"
-    })
-
-    User.create!({
-    email: "andre@gmail.com", 
-    password: "blahblah"
-    })
-
-    City.create!({
+    cities << City.create!({
     name: "Atlanta", 
     image:"https://i.imgur.com/ma1Eup5.jpg"
     })
 
-    City.create!({
+    cities << City.create!({
     name: "London", 
     image:"https://i.imgur.com/yahfVTm.jpg"
     })
 
-    City.create!({
+    cities << City.create!({
     name: "San Francisco", 
     image:"https://i.imgur.com/aFys868.jpg"
     })
 
+    users.each do |user|
+        cities.each do |city|
+            Post.create(
+                title: 'Nightlife',
+                description: 'fun bars, lots of live music, good clubs',
+                user_id: user.id,
+                city_id: city.id
+            )
+        end
+    end
+
+    # User.create!({
+    # email: "victoria@gmail.com", 
+    # password: "blahblah"
+    # })
+
+    # User.create!({
+    # email: "tom@gmail.com", 
+    # password: "blahblah"
+    # })
+
+    # User.create!({
+    # email: "andre@gmail.com", 
+    # password: "blahblah"
+    # })
+
     # Post.create!(
-    #     title: 'Parks',
-    #     description: 'great outdoor space, beautiful parks',
+    # title: 'Parks',
+    # description: 'great outdoor space, beautiful parks'
     # )
     
     # Post.create!(
-    #     title: 'Hotels',
-    #     description: 'amazing hotels - good prices and centrally located',
+    # title: 'Hotels',
+    # description: 'amazing hotels - good prices and centrally located'
     # )
     
     # Post.create!(
-    #     title: 'Nightlife',
-    #     description: 'fun bars, lots of live music, good clubs',
+    # title: 'Nightlife',
+    # description: 'fun bars, lots of live music, good clubs'
     # )
+
+    
+ 
 
 
 
