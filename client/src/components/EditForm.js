ecfff42a67fd73fdf568de553fc4ec48f599116f
victoriactemple <victoriactemple@gmail.com>
2017-11-03 10:26:16 -0400

    added changes to EditForm and PostForm. Padding is just a little off on the text area, but will do for now

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -5,20 +5,27 @@ import { Redirect } from 'react-router-dom'
 
 const FormContainer = styled.div`
 font-family: "Oxygen", sans-serif;
- padding-top: 100px;
-padding-left: 50px; 
+padding-top: 80px;
+padding-bottom: 80px;
 /* border: solid black 2px; */
 max-width: 700px;
 max-height: 500px;
 margin: 0 auto;
 /* pading: 0 1rem;  */
 text-align: center;
+background-color: white;
+border-radius: 5px;
+max-width: 400px;
+max-height: 400px;
+margin: 20px auto;
+border: 1px solid rgba(87, 87, 87, .2); 
+box-shadow: .5px .5px .5px .5px;
 `
 
 const Input = styled.input.attrs({
     
 	margin: props => props.size || '1em',
-	padding: props => props.size || '.5em'
+	padding: props => props.size || '2em'
 })`
 	color: black;
     font-family: "Oxygen", sans-serif;
@@ -41,6 +48,20 @@ background: white;
    &:hover{
         box-shadow: 1px 1px 2px;
     }
+`
+const TextArea = styled.textarea.attrs({
+    
+	margin: props => props.size || '.5em',
+	padding: props => props.size || '.5em'
+})`
+	color: black;
+    font-family: "Oxygen", sans-serif;
+	font-size: 0.75em;
+	border: 2px solid black;
+	border-radius: 3px;
+
+    margin: ${props => props.margin};
+	padding: ${props => props.padding};
 `
 
 
@@ -97,7 +118,7 @@ class EditForm extends Component {
                         placeholder={this.props.post.title} size="2em" />
                     </div>
                     <div>
-                        <Input onChange={this.handleChange} 
+                        <TextArea onChange={this.handleChange} 
                         name='description' 
                         type="text" 
                         placeholder={this.props.post.description}
