2db542084704139df497265309b14baddc4b5f4e
victoriactemple <victoriactemple@gmail.com>
2017-11-03 14:57:40 -0400

    added changes to POstList

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PostForm from './PostForm'
 import styled from 'styled-components'
-
   const Button = styled.button`
     cursor: pointer;
     background: white;
@@ -20,21 +19,28 @@ import styled from 'styled-components'
         box-shadow: 1px 1px 2px;
     }
    `
-
+   const Post = styled.div`
+   background-color:white;
+   width:50%;
+   margin:auto;
+   height:100px;
+   padding: 20px;
+   font-size:70px;
+   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+   border-radius: 25px;
+   `
 const PostList = (props) => {
     return (
         <div>
            <Button> <Link to={`/cities/${props.city.id}/posts`}>add review for {props.city.name}</Link></Button>
-
             {props.posts.map((post) => {
                 return (
                     <div>
-                   <Link to={`/cities/${props.city.id}/posts/${post.id}`}>{post.title} </Link><br />
+                  <Post src={props.city.image}> <Link to={`/cities/${props.city.id}/posts/${post.id}`}>{post.title} </Link></Post><br />
                     </div>
                 )
             })}
         </div>
     );
 };
-
 export default PostList;
