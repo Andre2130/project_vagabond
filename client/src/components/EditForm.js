eccdd10e1f7d0e9cf372753bfcad4a416a47343d
tombeau19 <thomas.beauregard@gmail.com>
2017-11-03 09:55:43 -0400

    fixed edit and brought in styling

--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -55,7 +55,7 @@ class EditForm extends Component {
     }
 
     componentWillMount() {
-        this.setState({ updatedPost: this.props.post })
+        this.setState({ updatedPost: this.props.posts })
     }
 
     handleChange = (event) => {
@@ -79,7 +79,7 @@ class EditForm extends Component {
     }
 
     render() {
-        if (this.state.redirectToPost === true) {
+        if (this.state.redirectToPost) {
             const city_id = this.state.updatedPost.city_id
             const id = this.state.updatedPost.id
             return (
@@ -94,13 +94,13 @@ class EditForm extends Component {
                         <Input onChange={this.handleChange} 
                         name='title' 
                         type="text" 
-                        placeholder={this.props.post.title} size="2em" />
+                        placeholder={this.props.posts.title} size="2em" />
                     </div>
                     <div>
                         <Input onChange={this.handleChange} 
                         name='description' 
                         type="text" 
-                        placeholder={this.props.post.description}
+                        placeholder={this.props.posts.description}
                         size="4em"/>
                     </div>
                     <div>
